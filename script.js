9b4af2423aca42f8de8ce1758bd8412b1de38edb
Helio-a-Tome <netsharkinformatica@gmail.com>
2021-08-19 12:30:43 -0300

    aulas Javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+/* var hora = new Date()
+
+horas = hora.getHours()
+
+if (horas < 12) {
+    alert('bom dia')
+
+
+} else if (horas >= 12 && horas < 18) {
+    alert('boa tardE')
+} else {
+    alert('boa noite')
+
+} */
+var nome = document.getElementById('exempl').innerHTML="Hélio Tomé"
+//var nome = 'Helio'
+//document.write(nome)
