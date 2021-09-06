e2de1c1da05804d47d3ff7a40eeb3b9095647538
Helio-A-Tome <netsharkinformatica@gmail.com>
2021-09-06 10:03:17 -0300

    studo case switch

---
+++ caseSwitch.js
@@ -0,0 +1,29 @@
+//estrutura
+let dia = 3
+let diaNome =''
+switch(dia){
+    case 1:
+    diaNome = 'domingo'
+    break
+    case 2:
+        diaNome ='segunda feira'
+        break
+        case 3:
+            diaNome = 'terça feira'
+            break
+        case 4:
+            diaNome  = 'quarta feira'
+            break
+    case 5:
+        diaNome = 'quinta feira'
+        break
+    case 6:
+        diaNome = 'sexta feira'
+        break
+    case 7:
+        diaNome = 'sabado'
+        break            
+
+
+}
+console.log(diaNome)
