5506017c2cf2d6ff896ea0271532ac382ef3c6ff
helio tomé <netshark@live.com>
2021-09-08 09:43:05 -0300

    manipulação de numeros

---
+++ metodonumbers.js
@@ -0,0 +1,8 @@
+let numero = 10.568976583
+let n = "20"
+//let numb = "56"
+//let resultado = Number(numb)//transforma string em numero
+//let res = numero.toString //transforma o numero em string
+//let resultado = numero.toFixed(2)//restinge as casas decimis ao numero que esta entre parenteses
+//let resultado = parseInt(n ) + numero//transforma string em numero pode ser parseFloat tambem
+console.log(  resultado)
