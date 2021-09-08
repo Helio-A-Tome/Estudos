0184e3b6dc58294fe8b14da462255ce37ec657aa
helio tomé <netshark@live.com>
2021-09-08 08:30:34 -0300

    aula manipulação de string

---
+++ metodoManipString.js
@@ -0,0 +1,16 @@
+let nome = "eu gosto de carros"
+//let resultado = nome.toUpperCase()//letras maiusculas
+//let resultado = nome.toLowerCase()//letras minusculas
+//let resultado = nome.concat(' e motos')//função pouco usada para concatenação
+//let resultado = nome.trim()//retira espaços vazios
+//let resultado = nome.charAt(7)//mostra a posição de determinado caractere em uma string
+let resultado = nome.split(' ')//transforma string em um array
+console.log(resultado)
+/*
+let resultado = nome.replace('carros','motos')
+console.log('nome: ',nome)
+console.log('resultado: ',resultado)
+
+// REPLACE substitui  a string pelo texto que eu precisar
+
+*/
