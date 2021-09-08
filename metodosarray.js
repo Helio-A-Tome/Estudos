386be7c6158cc9f0265608ded18e2688d5ac6542
helio tomé <netshark@live.com>
2021-09-08 11:14:36 -0300

    primeira aula sobre arrays

---
+++ metodosarray.js
@@ -0,0 +1,10 @@
+let lista = ['ovo','farinha','açucar','leite']
+//lista [0] ='ovos' // altera o nome do item do array
+
+//let res = lista.toString()// tranforma o array em string
+//let res = lista.join('-')//desfaz o array e separa os itens por algum caractere 
+//let res = lista.indexOf('farinha')//indica em qual posição esta o item no array
+//lista.pop() //retira oo ultimo item do array//shift()remove o primeiro item do array
+
+//let res = lista.push('suco de laranja')//adiciona mais um item ao array
+console.log(res)
