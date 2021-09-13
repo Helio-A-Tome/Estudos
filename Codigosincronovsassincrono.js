a5d34ac71bebc1b3c9b28e83420fa6583f011591
helio tomé <netshark@live.com>
2021-09-13 15:44:11 -0300

    Promises

--- Codigosincronovsassincrono.js
+++ Codigosincronovsassincrono.js
@@ -8,10 +8,27 @@ let sobrenome ='Tome'
 let temperatura = maquininha.pegarTemperatura()//assincrono
 let nomeCompleto=nome+''+sobrenome */
 /*********************************CallBack************************************* */
-function alertar(){
+/* function alertar(){
  console.log('opa, tudo bem?')
 }
 let nome = "Helio"
 setTimeout(alertar,2000)
 let sobrenome = "Tome"
-console.log("nome completo : " + nome + ' '+ sobrenome)
+console.log("nome completo : " + nome + ' '+ sobrenome) */
+/**********************************Promisses*************************************************** */
+function pegarTemperatura(){
+    return new Promise(function(resolve,reject){
+        console.log('pegando temperatura...')
+        setTimeout(function(){
+            resolve('40 na sombra')
+        },2000)
+    })
+    
+}
+let temp = pegarTemperatura()
+temp.then(function(resultado){
+    console.log('temperatura : '+ resultado)
+})
+temp.catch(function(error){
+    console.log('deu erro' + error)
+})
