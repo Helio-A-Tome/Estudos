182c18f3f55bbf643f3402247cdb806dad2c9be8
helio tomé <netshark@live.com>
2021-09-14 12:04:00 -0300

    fg

---
+++ fetchToPost.js
@@ -0,0 +1,17 @@
+async function inserirPost(){
+   
+    document.getElementById("posts").innerHTML='carregando...'
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        method :'POST',
+        body:JSON.stringify({
+            title:"titulo de teste",
+            body:"corpo de teste",
+            UserId: 4
+        }),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    let json = await req.json()
+    console.log(json)
+}
