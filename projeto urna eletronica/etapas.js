e6f4734d6a3e12f0e49100afefe1031a99c56654
helio tomé <netshark@live.com>
2021-09-28 15:27:08 -0300

    fim da parte visual 3ª aula

---
+++ projeto urna eletronica/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [{
+                numero: 38111,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'images/38111.png', legenda: 'Vereador', small: false }
+                ]
+
+
+
+            },
+            {
+                numero: 77222,
+                name: 'Ciclano da Silva',
+                partido: 'ZBH',
+                fotos: [
+                    { url: 'images/77222.png', legenda: 'Vereador', small: false }
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [{
+                numero: 84,
+                name: 'Beltrano de Oliveira',
+                partido: 'ABC',
+                vice: 'Belt',
+                fotos: [
+                    { url: 'images/84.png', legenda: 'Prefeito' },
+                    { url: 'images/84_2.png', legenda: 'Vice-Prefeito', small: false }
+                ]
+            },
+            {
+                numero: 99,
+                name: 'Selclan Silva',
+                partido: 'CBA',
+                vice: 'Selc',
+                fotos: [
+                    { url: 'images/99.png', legenda: 'Prefeito' },
+                    { url: 'images/99_2.png', legenda: 'Vice-Prefeito', small: false }
+                ]
+            }
+
+        ]
+    }
+
+]
