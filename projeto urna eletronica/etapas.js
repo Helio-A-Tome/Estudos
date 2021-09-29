07d19df5c330a1c37783f0e3419543f013c398e5
helio tomé <netshark@live.com>
2021-09-29 14:27:29 -0300

    Procurando candidato + voto nulo

--- projeto urna eletronica/etapas.js
+++ projeto urna eletronica/etapas.js
@@ -2,22 +2,22 @@ let etapas = [{
         titulo: 'VEREADOR',
         numeros: 5,
         candidatos: [{
-                numero: 38111,
+                numero: '38111',
                 nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos: [
-                    { url: 'images/38111.png', legenda: 'Vereador', small: false }
+                    { url: '38111.png', legenda: 'Vereador', small: false }
                 ]
 
 
 
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nome: 'Ciclano da Silva',
                 partido: 'ZBH',
                 fotos: [
-                    { url: 'images/77222.png', legenda: 'Vereador', small: false }
+                    { url: '77222.png', legenda: 'Vereador', small: false }
                 ]
             },
         ]
@@ -27,23 +27,23 @@ let etapas = [{
         titulo: 'Prefeito',
         numeros: 2,
         candidatos: [{
-                numero: 84,
+                numero: '84',
                 nome: 'Beltrano de Oliveira',
                 partido: 'ABC',
                 vice: 'Belt',
                 fotos: [
-                    { url: 'images/84.png', legenda: 'Prefeito' },
-                    { url: 'images/84_2.png', legenda: 'Vice-Prefeito', small: true }
+                    { url: '84.png', legenda: 'Prefeito' },
+                    { url: '84_2.png', legenda: 'Vice-Prefeito', small: true }
                 ]
             },
             {
-                numero: 99,
+                numero: '99',
                 nome: 'Selclan Silva',
                 partido: 'CBA',
                 vice: 'Selc',
                 fotos: [
-                    { url: 'images/99.png', legenda: 'Prefeito' },
-                    { url: 'images/99_2.png', legenda: 'Vice-Prefeito', small: true }
+                    { url: '99.png', legenda: 'Prefeito' },
+                    { url: '99_2.png', legenda: 'Vice-Prefeito', small: true }
                 ]
             }
 
--- projeto urna eletronica/script.js
+++ projeto urna eletronica/script.js
@@ -19,8 +19,10 @@ function comecarEtapa() {
         if (i === 0) {
             numeroHtml += '<div class="numero pisca"></div>'
 
+        } else {
+            numeroHtml += '<div class="numero"></div>'
         }
-        numeroHtml += '<div class="numero"></div>'
+
     }
 
     seuVotoPara.style.display = 'none'
@@ -31,10 +33,40 @@ function comecarEtapa() {
     numeros.innerHTML = numeroHtml
 
 }
-
+//Procurando candidato + voto nulo
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual]
+    let candidato = etapa.candidatos.filter((item) => {
+        if (item.numero === numero) {
+            return true
+        } else {
+            return false
+        }
 
-    alert("digitou")
+    })
+    if (candidato.length > 0) {
+        candidato = candidato[0]
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        descricao.innerHTML = `Nome: ${candidato.nome}</br>Partido: ${candidato.partido}`
+
+        let fotosHtml = ''
+
+        for (let i in candidato.fotos) {
+
+            fotosHtml += `<div class="d-1-imagem "> <img class="cand" src="images/${candidato.fotos[i].url}" alt=""> ${candidato.fotos[i].legenda} </div>`
+
+        }
+
+        lateral.innerHTML = fotosHtml
+
+    } else {
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        descricao.innerHTML = '<div class="avisoGrande pisca">VOTO NULO</div>'
+    }
+    //Procurando candidato + voto nulo
+
 
 }
 //Preenchendo Interface com números
